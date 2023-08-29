cd297c824972165e759cc42013df7b2b30cb2f84
Dario Quintero <darioaquintero@gmail.com>
2023-08-29 11:50:54 +0100

    FEAT: Further setup files

---
+++ .install/homebrew.sh
@@ -0,0 +1,5 @@
+sudo apt-get install build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/daquintero/.profile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc
---
+++ .install/ruby.sh
@@ -0,0 +1,3 @@
+brew install rbenv ruby-build
+rbenv install 3.1.4
+rbenv global 3.1.4
